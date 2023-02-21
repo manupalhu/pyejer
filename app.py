5a978edfc0fbe0e5793eb753e69ce656820ef85d
Manuel Palacios <93972984+manupalhu@users.noreply.github.com>
2023-02-21 11:37:47 +0100

    updated app

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hola mundo Flask' 
+def main():
+    return 'Hola cabesa' 
+
+@app.route('/how are you')
+def hello():
+    return 'Vamos tirando'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
